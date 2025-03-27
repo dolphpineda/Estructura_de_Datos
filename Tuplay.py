b981c2f434cc4e4870c23ad5c4e287d19c279cb1
dolphpineda <pineda0310@gmail.com>
2025-03-27 19:55:24 -0400

    commit

---
+++ Tuplay.py
@@ -0,0 +1,12 @@
+#Definir varias tuplas e imprimir sus elementos
+
+tupla=(1,2,3)
+fecha=(18,"Marzo",2024)
+puntos=(10,2)
+persona=("Garcia","Carlos",33)
+
+print(tupla);
+print(fecha)
+print(puntos)
+print(persona)
+
